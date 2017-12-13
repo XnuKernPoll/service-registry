c30189c09752686f8eec373979b5571a3a397069
sam <flatmapds@gmail.com>
2017-12-13 01:15:20 -0500

    added handler

--- src/service.ml
+++ src/service.ml
@@ -5,6 +5,9 @@ open Model
        
 module DataStore = DB.DataStore
 
+let path_split =
+  Str.split (Str.regexp "/")
+                     
 let ok_rep body =
   Server.respond_string ~status:`OK ~body:body () 
        
@@ -35,3 +38,26 @@ let list_services t ssid =
   DB.list_members t ssid >>= fun members ->
   let body = Fmt.strf "%a\n" (ServerSet.pp) members in
   ok_rep body 
+
+let continue_if_deserialized o f =
+  match o with
+  | Some x -> f x 
+  | None ->
+     Server.respond_string ~status:`Bad_request ~body:"unable to unmarshal data"
+  
+
+let basic_handler t conn req body =
+  let path = Request.uri (req)  |> Uri.path |> path_split in
+  let meth = Request.meth(req) in 
+  match (meth, path) with
+  | (`Get, ["catalog"; ssid]) ->  list_services t ssid
+  | (`Get, ["catalog"; ssid; id]) -> lookup t ssid id
+  | (`Delete, ["catalog"; ssid]) -> remove_server_set t ssid
+  | (`Put, ["catalog"; ssid]) -> create_server_set t ssid
+  | (`Delete, ["catalog"; ssid; id]) -> leave t ssid id
+  | (`POST, ["catalog"; ssid]) ->
+     let s = Cohttp_lwt_body.to_string body in
+     let o = Irmin.Type.decode_json service_t (Jsonm.decoder (`String s) ) in
+     continue_if_deserialized o (register ssid) 
+     
+  
