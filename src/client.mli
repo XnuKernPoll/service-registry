2626482f9310fa2b8b5f239f00df1b1cfee7cbf3
sam <flatmapds@gmail.com>
2017-12-19 04:10:53 -0500

    trying to merge

--- src/client.mli
+++
@@ -1,9 +0,0 @@
-open Model
-       
-val lookup: string -> string -> string -> (Service.t option) Lwt.t
-val list_services: string -> string -> ServerSet.t Lwt.t
-val leave: string -> string -> string -> unit Lwt.t
-val register: string -> string -> Service.t -> unit Lwt.t
-val create_server_set: string -> string -> unit Lwt.t
-val remove_server_set: string -> string -> unit Lwt.t
-
